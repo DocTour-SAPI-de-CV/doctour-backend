75a4b50464af9a5b04f557dc1acb13d854be4cf0
Henrique Munhoz <henriquemunhoz321@gmail.com>
2020-11-04 17:16:28 -0300

    fixed landing_controller

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,7 +1,6 @@
 class LandingController < ApplicationController
 before_action :authenticate_user!
 
-class LandingController < ApplicationController
   def index
     render json: { message: 'API ok!' }
   end
