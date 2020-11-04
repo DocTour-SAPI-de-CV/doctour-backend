b731950d187953fedcaa4448bbe543eff7776ab9
Andrea <andreavolzg@gmail.com>
2020-11-04 15:19:37 -0300

    create services table

--- db/migrate/20201103195143_create_services.rb
+++ db/migrate/20201103195143_create_services.rb
@@ -1,9 +1,10 @@
+# frozen_string_literal: true
+
 class CreateServices < ActiveRecord::Migration[6.0]
   enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
-
   def change
     create_table :services, id: :uuid, default: 'gen_random_uuid()' do |t|
-      t.string  :name, 
+      t.string  :name,
                 null: false,
                 index: { unique: true }
       t.float :price, null: false
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,9 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
+# incrementally modify your database, and then regenerate
+# this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
 # db:schema:load`. When creating a new database, `rails db:schema:load` tends to
@@ -8,21 +11,21 @@
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
-# It's strongly recommended that you check this file into your version control system.
+# It's strongly recommended that you check this
+# file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_03_195143) do
-
+ActiveRecord::Schema.define(version: 20_201_103_195_143) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "pgcrypto"
-  enable_extension "plpgsql"
+  enable_extension 'pgcrypto'
+  enable_extension 'plpgsql'
 
-  create_table "services", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "name", null: false
-    t.float "price", null: false
-    t.text "description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["name"], name: "index_services_on_name", unique: true
+  create_table 'services', id: :uuid, default: -> { 'gen_random_uuid()' },
+                           force: :cascade do |t|
+    t.string 'name', null: false
+    t.float 'price', null: false
+    t.text 'description'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['name'], name: 'index_services_on_name', unique: true
   end
-
 end
