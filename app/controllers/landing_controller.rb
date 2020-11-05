62f3d67ddef876f63305d4f87e1d966c5bf19379
Andrea <andreavolzg@gmail.com>
2020-11-05 16:11:04 -0300

    fix rubocop offenses

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class LandingController < ApplicationController
-before_action :authenticate_user!
+  before_action :authenticate_user!
 
   def index
     render json: { message: 'API ok!' }
