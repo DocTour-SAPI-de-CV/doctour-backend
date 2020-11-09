5d982d23b9356defa1dbd11689c6758b0c8490d5
Henrique Munhoz <henriquemunhoz321@gmail.com>
2020-11-09 16:15:14 -0300

    fixed controlle accout and people

--- app/controllers/account_controller.rb
+++ app/controllers/account_controller.rb
@@ -14,63 +14,29 @@ class AccountController < ApplicationController
   end
 
   def create
-    if params[:category].nil?
-      without_fields_error
-    elsif !check_category(params[:category])
-      render(json: {
-               Error: 'category must be admin, cliente, doctor or assistant'
-             })
-    else
-      create_account(params[:category])
-    end
+    account = Account.new(account_params)
+    render(json: { account: account }) if account.save
+
+    render(json: { error: account.errors })
   end
 
   def delete
     @account = Account.find(params[:id])
     @account.destroy!
+    
     render(json: { "account_id deleted": params[:id] })
   end
 
   def update
-    if params[:category].nil?
-      without_fields_error
-    elsif !check_category(params[:category])
-      render(json: {
-               Error: 'category must be admin, cliente, doctor or assistant'
-             })
-    else
-      @account = Account.find(params[:id])
-      update_category(params[:category])
-    end
+    account = Account.find(params[:id])
+    render(json: { account: account }) if account.update(account_params)
+      
+    render(json: { error: account.errors })
   end
 
   private
 
-  def check_category(category)
-    true if %w[admin client docter assistant].include? category.downcase
-  end
-
-  def create_account(category)
-    @account = Account.create(
-      category: category
-    )
-    @account.save!
-    render(json: { "account_id created": @account.id })
-  end
-
-  def without_fields_error
-    render(json: { Erro: 'Field category is required!' },
-           status: :unprocessable_entity)
-  end
-
-  def update_category(category)
-    if !@account.nil?
-      @account.update(category: category)
-      @account.save!
-      render(json: { "account_id updated": params[:id] })
-    else
-      render(json: { Error: 'Invalid ID, unable to update the account!' },
-             status: :unprocessable_entity)
-    end
+  def account_params
+    params.require(:account).permit(:category)
   end
 end
