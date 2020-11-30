016accbf8ed6286d64b8b02bfd6abc38f20cefdb
Henrique Munhoz <henriquemunhoz321@gmail.com>
2020-11-30 14:39:06 -0300

    added custom fileds on status endpoint

--- app/finders/doctors_status_finder.rb
+++ app/finders/doctors_status_finder.rb
@@ -4,23 +4,63 @@ class DoctorsStatusFinder < ApplicationFinder
   def find
     {
       0 => status_0,
-      1 =>  status_1,
+      1 => status_1,
       2 => status_2,
-      all: Doctor.all.ids
+      all: all
     }
   end
 
   private
 
   def status_0
-    Doctor.where(status: 0).ids
+    @doctors = Doctor.where(status: 0)
+    @results = []
+    @doctors.each do |doctor|
+      @results << {
+        name: doctor.full_name,
+        id: doctor.id
+      }
+    end
+
+    @results
   end
 
   def status_1
-    Doctor.where(status: 1).ids
+    @doctors = Doctor.where(status: 1)
+    @results = []
+    @doctors.each do |doctor|
+      @results << {
+        name: doctor.full_name,
+        id: doctor.id
+      }
+    end
+
+    @results
   end
 
   def status_2
-    Doctor.where(status: 2).ids
+    @doctors = Doctor.where(status: 2)
+    @results = []
+    @doctors.each do |doctor|
+      @results << {
+        name: doctor.full_name,
+        id: doctor.id
+      }
+    end
+
+    @results
+  end
+
+  def all
+    @doctors = Doctor.all
+    @results = []
+    @doctors.each do |doctor|
+      @results << {
+        name: doctor.full_name,
+        id: doctor.id
+      }
+    end
+
+    @results
   end
 end
