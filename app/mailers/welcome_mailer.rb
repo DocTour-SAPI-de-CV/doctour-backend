19b8a29891a750fd83f0a7dd7760383f846cf75e
Henrique Munhoz Silva Santos <henriquemunhoz321@gmail.com>
2020-12-02 12:49:08 +0000

    Welcome email

---
+++ app/mailers/welcome_mailer.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class WelcomeMailer < ApplicationMailer
+  RECOVERY_REDIRECT_URL = Rails.application.credentials.mailer[:redirect_url]
+
+  def send_email
+    @email = params[:email]
+    @full_name = params[:full_name]
+    @recovery_url = RECOVERY_REDIRECT_URL
+    
+    mail(to: @email, subject: 'Welcome to DocTour')
+  end
+end
