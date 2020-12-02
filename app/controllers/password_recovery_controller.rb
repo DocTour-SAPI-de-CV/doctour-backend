54b172d54a73a21ea299e4ea4ec6e2ec622c0c8c
Henrique Munhoz Silva Santos <henriquemunhoz321@gmail.com>
2020-12-02 21:41:48 +0000

    Cors

--- app/controllers/password_recovery_controller.rb
+++ app/controllers/password_recovery_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PasswordRecoveryController < ApplicationController
-  before_action :authenticate_user!, only: [:recovery]
+  before_action :authenticate_user, only: [:recovery]
 
   include ErrorSerializer
 
