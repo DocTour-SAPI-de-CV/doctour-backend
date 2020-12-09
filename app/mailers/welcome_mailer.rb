cc2030a30b01f580f0ab983ccbe85f1a12636ebe
Henrique Munhoz <henriquemunhoz321@gmail.com>
2020-12-09 10:01:48 -0300

    redirect url welcome mailer

--- app/mailers/welcome_mailer.rb
+++ app/mailers/welcome_mailer.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class WelcomeMailer < ApplicationMailer
-  RECOVERY_REDIRECT_URL = Rails.application.credentials.mailer[:redirect_url]
 
   def send_email
     @email = params[:email]
     @full_name = params[:full_name]
-    @recovery_url = RECOVERY_REDIRECT_URL
+    @redirect_url = "https://doctour.netlify.com/"
 
     mail(to: @email, subject: 'Welcome to DocTour')
   end
