b5a9c52e9ad4b595dc595b6ad6f4bbee06613173
Christopher Pottes <christopherpottesc@gmail.com>
2021-03-03 10:38:19 -0300

    Table drugs

--- db/migrate/20210219131135_create_drugs_historics.rb
+++ db/migrate/20210219131135_create_drugs_historics.rb
@@ -1,23 +1,23 @@
-# class CreateDrugsHistorics < ActiveRecord::Migration[6.0]
-#   def change
-#     create_table :drugs_historics, id: :uuid, default: 'gen_random_uuid()' do |t|
-#       t.references :non_pathologicals_historics,
-#                    type: :uuid,
-#                    null: false,
-#                    foreign_key: {
-#                      on_update: :cascade,
-#                      on_delete: :cascade
-#                    }
-#       t.references :drug,
-#                    type: :uuid,
-#                    null: false,
-#                    foreign_key: {
-#                      on_update: :cascade,
-#                      on_delete: :cascade
-#                    }
-#       t.string :quantity
+class CreateDrugsHistorics < ActiveRecord::Migration[6.0]
+  def change
+    create_table :drugs_historics, id: :uuid, default: 'gen_random_uuid()' do |t|
+      t.references :non_pathologicals_historics,
+                   type: :uuid,
+                   null: false,
+                   foreign_key: {
+                     on_update: :cascade,
+                     on_delete: :cascade
+                   }
+      t.references :drug,
+                   type: :uuid,
+                   null: false,
+                   foreign_key: {
+                     on_update: :cascade,
+                     on_delete: :cascade
+                   }
+      t.string :quantity
 
-#       t.timestamps
-#     end
-#   end
-# end
+      t.timestamps
+    end
+  end
+end
