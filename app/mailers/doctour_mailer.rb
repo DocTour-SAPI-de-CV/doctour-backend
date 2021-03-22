6861ee1a361919cfe3cbc1bf31e17a514d5e5c84
Anderson Souza <andersom.eugenio@gmail.com>
2021-03-22 14:55:53 -0300

    modelos de email finalizados

--- app/mailers/doctour_mailer.rb
+++ app/mailers/doctour_mailer.rb
@@ -1,17 +1,68 @@
 # frozen_string_literal: true
 
 class DoctourMailer < ApplicationMailer
+  before_action :get_params
+
   def greet
+    make_bootstrap_mail(
+      to: @email,
+      from: "no-reply@doctour.com",
+      subject: "Bem-vindo ao Doctour",
+    ) do |format|
+      format.html
+      # format.text
+    end
+  end
+
+  def attachment_added
+    make_bootstrap_mail(
+      to: @email,
+      from: "no-reply@doctour.com",
+      subject: "Documento anexado ao Doctour",
+    ) do |format|
+      format.html
+      # format.text
+    end
+  end
+
+  def password_changed
+    make_bootstrap_mail(
+      to: @email,
+      from: "no-reply@doctour.com",
+      subject: "Senha alterada com sucesso",
+    ) do |format|
+      format.html
+      # format.text
+    end
+  end
+
+  def password_recovery
+    make_bootstrap_mail(
+      to: @email,
+      from: "no-reply@doctour.com",
+      subject: "Recuperação de senha",
+    ) do |format|
+      format.html
+      # format.text
+    end
+  end
+
+  def password_register
+    make_bootstrap_mail(
+      to: @email,
+      from: "no-reply@doctour.com",
+      subject: "Bem-vindo ao Doctour",
+    ) do |format|
+      format.html
+      # format.text
+    end
+  end
+
+  private
+
+  def get_params
     @user = params[:user]
     @full_name = @user.account.people.full_name
     @email = @user.email
-    make_bootstrap_mail(
-      to: @email,
-      from: 'no-reply@doctour.com',
-      subject: 'Bem-vindo ao Doctour',
-      ) do |format|
-        format.html 
-       # format.text 
-      end
   end
 end
--- test/mailers/previews/doctour_mailer_preview.rb
+++ test/mailers/previews/doctour_mailer_preview.rb
@@ -2,4 +2,20 @@ class DoctourMailerPreview < ActionMailer::Preview
   def greet
     DoctourMailer.with(user: User.first).greet
   end
+
+  def password_changed
+    DoctourMailer.with(user: User.first).password_changed
+  end
+
+  def password_recovery
+    DoctourMailer.with(user: User.first).password_recovery
+  end
+
+  def password_register
+    DoctourMailer.with(user: User.first).password_register
+  end
+
+  def attachment_added
+    DoctourMailer.with(user: User.first).attachment_added
+  end
 end
