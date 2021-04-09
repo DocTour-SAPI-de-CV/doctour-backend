db2be7bd3a27238c3735531588bff3a6b98ae64c
Anderson Souza <andersom.eugenio@gmail.com>
2021-04-09 13:04:50 -0300

    finalizado send message e adicionado mensagens de debug no rails logger

--- app/channels/chat_channel.rb
+++ app/channels/chat_channel.rb
@@ -3,12 +3,15 @@
 class ChatChannel < ApplicationCable::Channel
   def subscribed
     stream_for current_user
-    puts "Usuario conectado: #{current_user}"
+    Rails.logger.info "Usuario conectado: #{current_user.email}"
   end
 
   def send_message(data)
-    from = current_user.id
-    to =data["to"]
+    from = current_user
+    to = User.find(data["to"])
     message = data["message"]
+    cm = ChatMessage.create!(message: message, from: from, to: to)
+    broadcast_to(to, { message: message, from_id: from.id })
+    Rails.logger.info "Mensage enviada de: #{from.email}, para: #{to.email}, mensagem: #{message}"
   end
 end
