8b0d939bf78bb55aa6e3b9b4e4f25645551e1764
Anderson Souza <andersom.eugenio@gmail.com>
2021-04-15 16:01:25 -0300

    apenas o campo readed é atualizavel e apenas de false para true

--- app/resources/chat_message_resource.rb
+++ app/resources/chat_message_resource.rb
@@ -9,9 +9,22 @@ class ChatMessageResource < JSONAPI::Resource
                }
   has_one :to, class_name: "ChatContact"
   has_one :from, class_name: "ChatContact"
-
+  before_update do
+    if ChatMessage.find(@model.id).readed == true
+      throw :abort
+    else
+      # no caso de no bd ja estar falso e querer setar como falso tbm
+      if @model.readed == false
+        throw :abort
+      end
+    end
+  end
   def self.records(options = {})
     current_user = options[:context][:current_user]
     current_user.sended_messages.or current_user.received_messages
+  end
+
+  def self.updatable_fields(context)
+    [:readed]
   end
 end
