90e5248747ba44bb7721daf0b63db4028ac12b0e
Anderson Souza <andersom.eugenio@gmail.com>
2021-04-22 15:36:15 -0300

    last message

--- app/resources/chat_contact_resource.rb
+++ app/resources/chat_contact_resource.rb
@@ -1,9 +1,15 @@
 class ChatContactResource < JSONAPI::Resource
   model_name "User"
-  attributes :email, :name, :category
+  attributes :email, :name, :category, :last_message
 
   def name
     "#{@model.account.people.first_name} #{@model.account.people.last_name}"
+  end
+
+  def last_message
+    current_user = context[:current_user]
+    msg = @model.sended_messages.where(to_id: current_user.id).or(@model.received_messages.where(from_id: current_user.id)).last
+    { message: msg.message, created_at: msg.created_at, type: msg.from_id == current_user.id ? "sended" : "received" } if msg
   end
 
   def category
