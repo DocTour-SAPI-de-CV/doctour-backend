923e0ba99b642715b706013486764d7224259a59
Christopher Pottes <christopherpottesc@gmail.com>
2022-03-14 20:11:31 -0300

    feat: add welcome_uploaded mailer

---
+++ app/mailers/welcome_upload_mailer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class WelcomeUploadMailer < ApplicationMailer
+  def send_email
+    @email = params[:email]
+    @full_name = params[:full_name]
+    @redirect_url = 'https://doctour-staging.netlify.com'
+
+    mail(to: @email, subject: 'Welcome to DocTour')
+  end
+end
