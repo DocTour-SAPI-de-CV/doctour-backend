77489ff06fdc77578c6abab33a1687cd6dbd3868
Christopher Pottes <christopherpottesc@gmail.com>
2022-04-06 20:42:47 -0300

    feat: add credentials aws doctour

--- config/routes.rb
+++ config/routes.rb
@@ -88,9 +88,9 @@ Rails.application.routes.draw do
   # delete 'assistant/:id', to: 'assistant#delete'
 
   # Services
-  # post 'service', to: 'service#create'
+  post 'service', to: 'service#create'
   # put 'service/:id', to: 'service#update'
-  # get 'service', to: 'service#show'
+  get 'service', to: 'service#show'
   # get 'service/:id', to: 'service#index'
   # delete 'service/:id', to: 'service#delete'
 
