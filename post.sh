f032d2050ca563e38cb55066490107b52fe0bf2a
deh-alba <andre90012002@gmail.com>
2023-03-06 22:47:37 -0300

    add mapping external run_deh and change port

--- post.sh
+++ post.sh
@@ -2,6 +2,6 @@
 
 
 aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 432968470264.dkr.ecr.us-east-2.amazonaws.com
-docker build -t doctour-api .
+docker build --no-cache -t doctour-api .
 docker tag doctour-api:latest 432968470264.dkr.ecr.us-east-2.amazonaws.com/doctour-api:latest
 docker push 432968470264.dkr.ecr.us-east-2.amazonaws.com/doctour-api:latest
--- run_deh.sh
+++ run_deh.sh
@@ -1,1 +1,2 @@
-docker build -t doctour-backend . && docker run -p 0.0.0.0:5000:5000 -it doctour-backend
+docker build -t doctour-backend . && docker run -i -t -v "$(pwd)"/:/doctour -p 0.0.0.0:5000:3000 -it doctour-backend
+
