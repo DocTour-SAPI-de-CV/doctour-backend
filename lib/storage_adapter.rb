b393760918758607b452116ee0fc23733dfb4c75
GabrielGarza82 <ggarza@doctour.com.mx>
2025-09-08 15:55:46 -0300

    Create storage_adapter.rb - abstaction layer
    This abstraction layer will allow you to switch between AWS S3 and Azure Blob Storage by simply changing the STORAGE_PROVIDER environment variable, making your application truly cloud-agnostic.

---
+++ lib/storage_adapter.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true  
+  
+class StorageAdapter  
+  class << self  
+    def upload(file, file_path)  
+      provider.upload(file, file_path)  
+    end  
+  
+    def delete(file_path)  
+      provider.delete(file_path)  
+    end  
+  
+    def get_url(file_path)  
+      provider.get_url(file_path)  
+    end  
+  
+    private  
+  
+    def provider  
+      @provider ||= case ENV.fetch('STORAGE_PROVIDER', 'aws')  
+                    when 'aws'  
+                      AwsS3Storage.new  
+                    when 'azure'  
+                      AzureBlobStorage.new  
+                    else  
+                      raise "Unsupported storage provider: #{ENV['STORAGE_PROVIDER']}"  
+                    end  
+    end  
+  end  
+end
